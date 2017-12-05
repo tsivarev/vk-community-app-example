f337881067bb0c8d71823b872cc7dea9fe510cd3
Gilevich Petr <unvir@yandex.ru>
2017-12-06 00:30:08 +0300

    Added page with getting access

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var app = {
     pages: {
         INSTALL: document.getElementById('page1'),
-        START: document.getElementById('page2')/*,
-        GET_ACCESS: 0,
-        PICK_PHOTO: 0,
+        START: document.getElementById('page2'),
+        PICK_PHOTO: document.getElementById('page3')/*,
         CONFIRM: 0*/
     },
     show: function(page){
@@ -18,7 +17,7 @@ var app = {
 };
 
 window.addEventListener('load', function(){
-    VK.init( null, null, '5.69');
+    VK.init(null, null, '5.69');
 
     var queryString = window.location.href;
 
@@ -27,6 +26,20 @@ window.addEventListener('load', function(){
     else
         app.show(app.pages.START);
 
+
+    document.getElementById('btn-getAccess')
+            .addEventListener('click', function(e){
+
+        e.preventDefault();
+        VK.callMethod("showSettingsBox", 4); // Доступ к фотографиям
+
+        VK.addCallback('onSettingsChanged', onSuccess);
+
+        function onSuccess(){
+            VK.removeCallback('onSettingsChanged', onSuccess);
+            app.show(app.pages.PICK_PHOTO);
+        }
+    });
 
     function getGroupId(str){
         var pos = str.indexOf('group_id=');
