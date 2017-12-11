f75d69dd83b1a4be6db1d2ac62802be800c6eca8
Petr Gilevich <unvir@yandex.ru>
2017-12-11 10:44:25 +0300

    Release refactoring

--- app.js
+++ app.js
@@ -1,166 +1,165 @@
 var app = {
-  API_VERSION: '5.69',
-  API_SETTINGS_SCOPE_PHOTOS: 4,
-  VIEWER_DEVICE_MOBILE: 'mobile',
-  PAGES: {
-    INSTALL: document.getElementById('page-install'),
-    START: document.getElementById('page-start'),
-    PICK_PHOTO: document.getElementById('page-pick-photo'),
-    ENTER_DESCRIPTION: document.getElementById('page-enter-text')
-  },
+    API_VERSION: '5.69',
+    API_SETTINGS_SCOPE_PHOTOS: 4,
+    VIEWER_DEVICE_MOBILE: 'mobile',
+    PAGES: {
+        INSTALL: document.getElementById('page-install'),
+        START: document.getElementById('page-start'),
+        PICK_PHOTO: document.getElementById('page-pick-photo'),
+        ENTER_DESCRIPTION: document.getElementById('page-enter-text')
+    },
 
-  appId: 0,
-  groupId: 0,
+    appId: 0,
+    groupId: 0,
 
-  show: function(page) {
-    app.hideAll();
-    page.style.display = 'block';
+    show: function (page) {
+        app.hideAll();
+        page.style.display = 'block';
 
-    switch (page) {
+        switch (page) {
+            case app.PAGES.START:
+                var requestData = {
+                    'user_id': sessionStorage.getItem('viewerId')
+                };
 
-      case app.PAGES.START:
-        var requestData = {
-          'user_id': sessionStorage.getItem('viewerId')
-        };
+                VK.api('account.getAppPermissions', requestData, function (data) {
+                    var btnGetAccessElem = document.getElementById('btn-get-access');
 
-        VK.api('account.getAppPermissions', requestData, function(data) {
+                    if (data.response & app.API_SETTINGS_SCOPE_PHOTOS) {
+                        btnGetAccessElem.innerHTML = 'Доступ предоставлен. Продолжить';
+                        btnGetAccessElem.addEventListener('click', function (event) {
+                            event.preventDefault();
+                            app.show(app.PAGES.PICK_PHOTO);
+                        });
+                    } else {
+                        btnGetAccessElem.addEventListener('click', getAccess);
+                    }
 
-          var btnGetAccessElem = document.getElementById('btn-get-access');
+                    function getAccess(event) {
+                        event.preventDefault();
 
-          if (data.response & app.API_SETTINGS_SCOPE_PHOTOS) {
+                        VK.callMethod('showSettingsBox', app.API_SETTINGS_SCOPE_PHOTOS);
+                        VK.addCallback('onSettingsChanged', onSuccess);
 
-            btnGetAccessElem.innerHTML = 'Доступ предоставлен. Продолжить';
-            btnGetAccessElem.addEventListener('click', function(event) {
-                event.preventDefault();
-                app.show(app.PAGES.PICK_PHOTO);
-            });
-          } else {
-            btnGetAccessElem.addEventListener('click', getAccess);
-          }
+                        function onSuccess() {
+                            VK.removeCallback('onSettingsChanged', onSuccess);
+                            app.show(app.PAGES.PICK_PHOTO);
+                        }
+                    }
+                });
+                break;
 
-          function getAccess(event) {
-            event.preventDefault();
+            case app.PAGES.PICK_PHOTO:
+                document.getElementById('container-photos').innerHTML = '';
 
-            VK.callMethod('showSettingsBox', app.API_SETTINGS_SCOPE_PHOTOS);
-            VK.addCallback('onSettingsChanged', onSuccess);
+                var requestData = {
+                    'owner_id': sessionStorage.getItem('viewerId'),
+                    'count': 5,
+                    'skip_hidden': 1
+                };
 
-            function onSuccess() {
-              VK.removeCallback('onSettingsChanged', onSuccess);
-              app.show(app.PAGES.PICK_PHOTO);
-            }
-          }
-        });
-        break;
+                var listElem = document.createElement('ul');
+                listElem.classList.add('list-photo');
 
-      case app.PAGES.PICK_PHOTO:
-        document.getElementById('container-photos').innerHTML = '';
+                VK.api('photos.getAll', requestData, function (data) {
+                    data.response.items.forEach(function (photo) {
+                        var liElem = document.createElement('li');
+                        var imgElem = document.createElement('img');
 
-        var requestData = {
-          'owner_id': sessionStorage.getItem('viewerId'),
-          'count': 5,
-          'skip_hidden': 1
-        };
+                        imgElem.src = photo.photo_130;
+                        imgElem.onclick = app.onPhotoPicked;
+                        imgElem.photoUrl = photo.photo_604;
+                        imgElem.photoId = photo.id;
 
-        var listElem = document.createElement('ul');
-        listElem.classList.add('list-photo');
+                        liElem.appendChild(imgElem);
+                        listElem.appendChild(liElem);
+                    });
 
-        VK.api('photos.getAll', requestData, function(data) {
+                    document.getElementById('container-photos').appendChild(listElem);
+                });
+                break;
 
-          data.response.items.forEach(function(photo) {
-            var liElem = document.createElement('li');
-            var imgElem = document.createElement('img');
+            case app.PAGES.ENTER_DESCRIPTION:
+                document.getElementById('textarea-post-description').value = '';
+                break;
+        }
+    },
 
-            imgElem.src = photo.photo_130;
-            imgElem.onclick = app.onPhotoPicked;
-            imgElem.photoUrl = photo.photo_604;
-            imgElem.photoId = photo.id;
+    onPhotoPicked: function (event) {
+        event.preventDefault();
 
-            liElem.appendChild(imgElem);
-            listElem.appendChild(liElem);
-          });
+        sessionStorage.setItem('photoUrl', event.target.photoUrl);
+        sessionStorage.setItem('photoId', event.target.photoId);
 
-          document.getElementById('container-photos').appendChild(listElem);
-        });
-        break;
+        app.show(app.PAGES.ENTER_DESCRIPTION);
+    },
 
-      case app.PAGES.ENTER_DESCRIPTION:
-        document.getElementById('textarea-post-description').value = '';
-        break;
-    }
-  },
+    hideAll: function () {
+        for (var i in app.PAGES) {
+            app.PAGES[i].style.display = 'none';
+        }
+    },
 
-  onPhotoPicked: function(event) {
-    event.preventDefault();
+    getUrlParameter: function (name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
 
-    sessionStorage.setItem('photoUrl', event.target.photoUrl);
-    sessionStorage.setItem('photoId', event.target.photoId);
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(location.search);
 
-    app.show(app.PAGES.ENTER_DESCRIPTION);
-  },
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    },
 
-  hideAll: function() {
-    for (var i in app.PAGES) {
-      app.PAGES[i].style.display = 'none';
-    }
-  },
+    init: function () {
+        app.appId = app.getUrlParameter('api_id');
+        app.groupId = app.getUrlParameter('group_id');
 
-  getUrlParameter: function(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  },
-
-  init: function() {
-    app.appId = app.getUrlParameter('api_id');
-    app.groupId = app.getUrlParameter('group_id');
-
-    document.getElementById('btn-include-app')
+        document.getElementById('btn-include-app')
             .href = 'https://vk.com/add_community_app?aid=' + app.appId;
 
-    VK.init(null, null, app.API_VERSION);
+        VK.init(null, null, app.API_VERSION);
 
-    sessionStorage.setItem('viewerId',
-                          app.getUrlParameter('viewer_id'));
+        sessionStorage.setItem('viewerId',
+            app.getUrlParameter('viewer_id'));
 
-    if (app.groupId == 0) {
-      app.show(app.PAGES.INSTALL);
-    } else {
-      app.show(app.PAGES.START);
-    }
-
-    document.getElementById('btn-back-to-photos').addEventListener('click', function(event) {
-        event.preventDefault();
-
-        app.show(app.PAGES.PICK_PHOTO);
-    });
-    document.getElementById('btn-submit').addEventListener('click', function (event) {
-        event.preventDefault();
-
-        if (app.getUrlParameter('viewer_device').toLowerCase() == app.VIEWER_DEVICE_MOBILE) {
-
-          VK.callMethod('shareBox',
-                        'https://vk.com/app' + app.appId,
-                        sessionStorage.getItem('photoUrl'),
-                        document.getElementById('textarea-post-description').value);
+        if (app.groupId === 0) {
+            app.show(app.PAGES.INSTALL);
         } else {
-          var photoRawId = 'photo' + sessionStorage.getItem('viewerId') +
-                          '_' + sessionStorage.getItem('photoId');
-          var appLink = 'https://vk.com/app' + app.appId + '_-' + app.groupId;
-          var requestData = {
-            'owner_id': sessionStorage.getItem('viewerId'),
-            'message': document.getElementById('textarea-post-description').value,
-            'attachments':  photoRawId + ',' + appLink;
-          };
-
-          VK.api('wall.post', requestData);
+            app.show(app.PAGES.START);
         }
-    });
-  }
+
+        document.getElementById('btn-back-to-photos').addEventListener('click', function (event) {
+            event.preventDefault();
+
+            app.show(app.PAGES.PICK_PHOTO);
+        });
+
+        document.getElementById('btn-submit').addEventListener('click', function (event) {
+            event.preventDefault();
+
+            var viewerDevice = app.getUrlParameter('viewer_device');
+            if (viewerDevice && viewerDevice === app.VIEWER_DEVICE_MOBILE) {
+                VK.callMethod('shareBox',
+                    'https://vk.com/app' + app.appId,
+                    sessionStorage.getItem('photoUrl'),
+                    document.getElementById('textarea-post-description').value);
+            } else {
+                var photoRawId = 'photo' + sessionStorage.getItem('viewerId') +
+                    '_' + sessionStorage.getItem('photoId');
+                var appLink = 'https://vk.com/app' + app.appId + '_-' + app.groupId;
+
+                var requestData = {
+                    'owner_id': sessionStorage.getItem('viewerId'),
+                    'message': document.getElementById('textarea-post-description').value,
+                    'attachments': photoRawId + ',' + appLink
+                };
+
+
+                VK.api('wall.post', requestData);
+            }
+        });
+    }
 };
 
-window.addEventListener('load', function() {
-  app.init();
+window.addEventListener('load', function () {
+    app.init();
 });
